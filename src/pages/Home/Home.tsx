66fceea37a3263608775f2da942bcc9c6bcc3d59
ReWar1311 <prashantrewarofficial@gmail.com>
2025-03-05 10:29:37 +0530

    explore button revert

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import './Home.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import Alert from '../../components/Alert/Alert';
