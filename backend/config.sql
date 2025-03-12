79605e23bcae2e9fc8747ee22c192689c92cdf57
ReWar1311 <prashantrewarofficial@gmail.com>
2025-03-12 22:34:36 +0530

    cleaned

--- backend/config.sql
+++
@@ -1,8 +0,0 @@
--- USER TABLE CREATION --
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,   -- A unique identifier for each user
-    name VARCHAR(255) NOT NULL,           -- User's name (string, max 255 characters)
-    contact VARCHAR(15),                  -- User's contact number (string, max 15 characters)
-    email VARCHAR(255) NOT NULL UNIQUE,   -- User's email (string, max 255 characters) with unique constraint
-    password VARCHAR(255) NOT NULL        -- User's password (string, max 255 characters)
-);
